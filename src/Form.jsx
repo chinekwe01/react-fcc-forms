71f653647c31af9dc1b927393b160e9d89f4ce4f
chinekwe01 <chinekwe_clever@outlook.com>
2023-06-27 19:01:47 +0100

    initial commit

---
+++ src/Form.jsx
@@ -0,0 +1,26 @@
+/*-------------------------------------------------------------------
+|  🐼 React FC Form
+|
+|  🦝 Todo: CREATE AN AWESOME AND MAINTAINABLE FORM COMPONENT 
+|
+|  🐸 Returns:  JSX
+*-------------------------------------------------------------------*/
+
+import { Input } from './components'
+import { FormProvider, useForm } from 'react-hook-form'
+import {
+  name_validation,
+  desc_validation,
+  email_validation,
+  num_validation,
+  password_validation,
+} from './utils/inputValidations'
+import { useState } from 'react'
+import { GrMail } from 'react-icons/gr'
+import { BsFillCheckSquareFill } from 'react-icons/bs'
+
+export const Form = () => {
+  return (
+    <div className="text-center mt-5">Welcome to React Forms Tutorial!</div>
+  )
+}
