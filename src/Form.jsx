782b76ba1ee857db9c2cf10df9eee0e21d414076
chinekwe01 <chinekwe_clever@outlook.com>
2023-06-27 22:23:38 +0100

    handling successful form submission

--- src/Form.jsx
+++ src/Form.jsx
@@ -21,9 +21,12 @@ import { BsFillCheckSquareFill } from 'react-icons/bs'
 
 export const Form = () => {
   const methods = useForm()
+  const [success, setSuccess] = useState(false)
 
   const onSubmit = methods.handleSubmit(data => {
     console.log(data)
+    methods.reset()
+    setSuccess(true)
   })
 
   return (
@@ -31,23 +34,22 @@ export const Form = () => {
       <form
         onSubmit={e => e.preventDefault()}
         noValidate
+        autoComplete="off"
         className="container"
       >
         <div className="grid gap-5 md:grid-cols-2">
-          <Input
-            label="name"
-            type="text"
-            id="name"
-            placeholder="type your name..."
-          />
-          <Input
-            label="password"
-            type="password"
-            id="password"
-            placeholder="type your password..."
-          />
+          <Input {...name_validation} />
+          <Input {...email_validation} />
+          <Input {...num_validation} />
+          <Input {...password_validation} />
+          <Input {...desc_validation} className="md:col-span-2" />
         </div>
         <div className="mt-5">
+          {success && (
+            <p className="flex items-center gap-1 mb-5 font-semibold text-green-500">
+              <BsFillCheckSquareFill /> Form has been submitted successfully
+            </p>
+          )}
           <button
             onClick={onSubmit}
             className="flex items-center gap-1 p-5 font-semibold text-white bg-blue-600 rounded-md hover:bg-blue-800"
